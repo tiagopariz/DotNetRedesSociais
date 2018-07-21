517e1191559dab910739bbd6b82441c91405f160
tiagopariz <tiagopariz@gmail.com>
2018-07-21 10:54:16 -0300

    Projeto Web Identity

---
+++ DotNetRedesSociais.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetRedesSociais.Web.Startup))]
+namespace DotNetRedesSociais.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
